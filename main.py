0c559c688f353b6e6d3561150aebde20586bd4c6
hadyjimy <hadyg3733@gmail.com>
2022-12-17 23:12:09 +0200

    last modified

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import requests
 import bs4
-url = 'https://www.amazon.eg/-/en/b?bbn=21832872031&rh=n%3A18018102031%2Cn%3' \
-      'A21832872031%2Cn%3A21832907031&dc&qid=1670526756&rnid=2183' \
-      '2872031&ref=lp_21832872031_nr_n_5&node=21832907031'
+url = 'https://www.amazon.eg/-/en/b?bbn=21832872031&rh=n%3A18018102031%2Cn%3A21832872031%2Cn%3A21832907031&dc&qid=1670526756&rnid=2183 2872031&ref=lp_21832872031_nr_n_5&node=21832907031'
 laptopsprices=[]
 laptopsnames=[]
 page=requests.get(url)
-src=page.content
-print(src)
-soup = bs4.BeautifulSoup(src, "html.parser")
-print(soup)
+soup = bs4.BeautifulSoup(page.content,"html.parser")
 laptopsnames=soup.find_all("span",{"class":"a-truncate acs-product-block__contributor a-size-base"})
-print(laptopsnames)
-for i in range(len(laptopsnames)):
-    laptopsnames.append(laptopsnames[i].text)
-print(laptopsnames)
 laptopsprices=soup.find_all("span",{"class":"a-price-whole"})
-print(laptopsprices)
-for i in range(len(laptopsprices)):
-   laptopsprices.append(laptopsprices[i].text)
-print(laptopsprices)
+# print(laptopsnames)
+for i in range(len(laptopsnames)):
+    laptopsnames.append(laptopsnames[i])
+    laptopsprices.append(laptopsprices[i])
+
+
+for i in range(len(laptopsnames)):
+    print(f"laptob name : {laptopsnames[i].text.strip()}")
+    print(f"laptob price : {laptopsprices[i].text}")
+    print("*"*50)
+
